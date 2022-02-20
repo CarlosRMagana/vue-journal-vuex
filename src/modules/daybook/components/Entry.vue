f1c03008471cdd3b182e994ae8a0bd0ddd0e7fcc
CarlosRMagana <92707043+CarlosRMagana@users.noreply.github.com>
2022-02-19 18:55:12 -0600

    First commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="entry-container mb-3 pointer p-2" @click="$router.push({ name: 'entry', params: { id: 10 }})">
+        <!-- Title -->
+        <div class="entry-title d-flex"></div>
+        <span class="text-success fs-5 fw-bold">17</span>
+        <span class="mx-1 fs-5">Febrero</span>
+        <span class="mx-2 fw-light">2021, jueves</span>
+    
+
+        <div class="entry-description">
+            Irure ullamco et minim incididunt cupidatat. Nisi nostrud eu amet velit. Fugiat cupidatat sunt nisi fugiat. Quis cupidatat velit eiusmod esse.
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date.toDateString(),
+            text: 'Nisi ea ipsum ex pariatur do est esse ut. Excepteur nostrud quis ad ex do id mollit consectetur id aliquip aute. Et occaecat duis adipisicing labore magna qui consectetur. Deserunt anim labore nisi nulla aute irure nostrud nostrud et laboris pariatur aliqua commodo exercitation. Adipisicing et fugiat minim nostrud mollit consectetur culpa elit.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date.toDateString(),
+            text: 'Veniam do elit eiusmod et elit reprehenderit voluptate aliquip quis amet. Aliqua cupidatat ipsum dolore voluptate Lorem aliquip non aliquip fugiat nostrud ea occaecat quis officia. Excepteur in est aliqua consequat ad aliqua nisi labore. Esse nisi exercitation incididunt cillum sunt ullamco aliquip. Quis voluptate sunt nostrud culpa aliquip nostrud cupidatat nisi eu aliqua quis. Nisi ut duis amet esse culpa esse reprehenderit. Dolor consequat excepteur anim veniam et cupidatat qui deserunt nisi sit officia non mollit velit.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date.toDateString(),
+            text: 'Laboris occaecat mollit dolore veniam adipisicing est eu occaecat dolor anim commodo. Non pariatur ex consectetur enim aliquip fugiat laboris nostrud. Esse commodo eiusmod in quis deserunt nisi. Cupidatat minim qui aliqua velit dolor ex nisi. Enim amet quis anim velit in velit fugiat proident elit ullamco aliquip nostrud duis. Eu Lorem commodo ex dolor qui elit ad officia et veniam aute.',
+            picture: null,
+        },
+    ]
+})
